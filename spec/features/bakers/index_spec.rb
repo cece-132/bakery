b183a4c2871c721ca880a7e70b2e78222e7bff46
Candace Eckels <candace.eckels@gmail.com>
2023-05-27 12:28:49 -0600

    Added testing and links to bakers show page

--- spec/features/bakers/index_spec.rb
+++ spec/features/bakers/index_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Bakers' do
+
   describe 'index' do
     it 'lists all the bakers' do
       b1 = Baker.create!(name: 'Timmy')
+      b2 = Baker.create!(name: 'Tina')
 
       visit bakers_path
 
       expect(page).to have_content(b1.name)
+      expect(page).to have_content(b2.name)
+    end
+
+    it 'the bakers name links to a show page' do
+      b1 = Baker.create!(name: 'Timmy')
+      visit bakers_path
+
+      expect(page).to have_link("#{b1.name}")
     end
   end
+
 end
